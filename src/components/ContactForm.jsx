78e814bf66d9b21613795ef12b57a6927fa77062
Brice Chen <bchen72@my.bcit.ca>
2024-04-20 22:42:26 -0700

    finish contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,43 +1,74 @@
-import React from "react";
+import { useState } from "react";
 
-export default function Contact() {
-  const [result, setResult] = React.useState("");
+export default function ContactForm() {
+  const [result, setResult] = useState("");
+  const [name, setName] = useState("");
+  const accessKey = "d6f78704-093d-4cdb-93bc-5d1628d51e37";
 
   const onSubmit = async (event) => {
     event.preventDefault();
     setResult("Sending....");
     const formData = new FormData(event.target);
 
-    formData.append("access_key", "d6f78704-093d-4cdb-93bc-5d1628d51e37");
+    formData.append("access_key", accessKey);
+    formData.append("subject", name+ " has submitted a form from your website."); 
 
-    const response = await fetch("https://api.web3forms.com/submit", {
-      method: "POST",
-      body: formData
-    });
+    try {
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData,
+      });
 
-    const data = await response.json();
+      const data = await response.json();
 
-    if (data.success) {
-      setResult("Form Submitted Successfully");
-      event.target.reset();
-    } else {
-      console.log("Error", data);
-      setResult(data.message);
+      if (data.success) {
+        setResult("Form Submitted Successfully");
+        event.target.reset();
+      } else {
+        console.log("Error", data);
+        setResult(data.message);
+      }
+    } catch (error) {
+      console.error("Error submitting form:", error);
+      setResult("Error submitting form. Please try again.");
     }
+  };
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
   };
 
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <input type="text" name="name" required/>
-        <input type="email" name="email" required/>
-        <textarea name="message" required></textarea>
-
-        <button type="submit">Submit Form</button>
-
+        <input
+          type="hidden"
+          name="access_key"
+          value={accessKey}
+        />
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={name}
+          onChange={handleNameChange}
+          required
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="E-mail"
+          required
+        />
+        <textarea
+          name="message"
+          required
+        />
+        <button type="submit">
+          Submit Form
+        </button>
       </form>
       <span>{result}</span>
-
     </div>
   );
 }
