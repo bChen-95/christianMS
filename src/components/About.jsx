8d1716a05f08679835634f7cd2cb8a335398cdc2
Brice Chen <bchen72@my.bcit.ca>
2024-04-25 13:33:19 -0700

    removed unused classnames

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,7 +19,7 @@ function About() {
     return (
         <div className="about-wrapper">
             <div className="about-banner">
-                {<img src={tempPic} alt="About Banner" className="banner-image parallax-effect" /* style={{ transform: `translateY(${scrollOffset * 0.5}px)` }} *//> }
+                <img src={tempPic} alt="About Banner" className="banner-image parallax-effect" />
                 <div className="banner-content">
                     <h1 className="banner-title">About</h1>
                     <p className="banner-message">lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>
