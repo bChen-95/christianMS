269580f9e40b0935f5c16b06a4d6ccb7a354b446
Brice Chen <bchen72@my.bcit.ca>
2024-04-25 15:44:18 -0700

    add grid to info section, add section for CTA

--- src/components/InfoSection.jsx
+++ src/components/InfoSection.jsx
@@ -2,14 +2,22 @@ import infoData from '../json/info.json';
 
 function InfoSection() {
     return (
-        <div>
-            {infoData.info.map(item => (
-                <div key={item.id}>
-                    <img src={item.image} alt="Info" />
-                    <h2>{item.title}</h2>
-                    <p>{item.paragraph}</p>
-                </div>
-            ))}
+        <div className="infoSectionWrapper">
+            <div className="infoGrid">
+                {infoData.info.map(item => (
+                    <div key={item.id}>
+                        <img src={item.image} alt="Info" />
+                        <h2>{item.title}</h2>
+                        <p>{item.paragraph}</p>
+                    </div>
+                ))}
+            </div>
+
+            <div className="cta">
+                <h2>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. <a href="/contact">Nullam</a>
+                </h2>
+            </div>
         </div>
     );
 }
