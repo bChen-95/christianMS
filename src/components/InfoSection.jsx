fd75e4f0364268b49f5c7b256e2c475bb855ca2f
Brice Chen <brice_chen@hotmail.com>
2024-04-29 14:30:40 -0700

    change grid to flex, finish info layout

--- src/components/InfoSection.jsx
+++ src/components/InfoSection.jsx
@@ -3,9 +3,9 @@ import infoData from '../json/info.json';
 function InfoSection() {
     return (
         <div className="infoSectionWrapper">
-            <div className="infoGrid">
+            <div className="infoFlex">
                 {infoData.info.map(item => (
-                    <div key={item.id}>
+                    <div className="infoItems" key={item.id}>
                         <img src={item.image} alt="Info" />
                         <h2>{item.title}</h2>
                         <p>{item.paragraph}</p>
