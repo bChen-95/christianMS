9261cff69f1c82b9bffe2ebad29df60e2cecb24e
Brice Chen <bchen72@my.bcit.ca>
2024-05-26 13:54:25 -0700

    slight adjustments to code layout for better readability

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,20 +1,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Header from './components/Header';
+import Footer from './components/Footer';
+
 import PageHome from './page/PageHome';
 import PageAbout from './page/PageAbout';
-import Footer from './components/Footer';
+
 
 
 
 function AppRouter() {
 return (
     <BrowserRouter>
-        <Header />
-      <Routes>
-        <Route path="/" exact element={<PageHome/>}  />
+      <Header />
+        <Routes>
+          <Route path="/" exact element={<PageHome/>}  />
           <Route path="/about" element={<PageAbout />} />
-      </Routes>
+        </Routes>
       <Footer />
     </BrowserRouter>
   );
